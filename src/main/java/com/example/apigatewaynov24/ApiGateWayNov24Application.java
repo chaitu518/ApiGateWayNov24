630f3506c5d2e0c94ef852c1c470a08a47c6581d
saichaitanya518 <saichaitanya518@gmail.com>
2024-12-27 21:32:02 +0530

    first commit

---
+++ src/main/java/com/example/apigatewaynov24/ApiGateWayNov24Application.java
@@ -0,0 +1,13 @@
+package com.example.apigatewaynov24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayNov24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayNov24Application.class, args);
+	}
+
+}
